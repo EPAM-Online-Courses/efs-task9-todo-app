df8859f4460999da95c34eee4a8a43f769ee05ab
pawel_labaj <pawel_labaj@epam.com>
2021-04-20 15:58:25 +0200

    #1 Task template

--- src/main/java/efs/task/todoapp/repository/TaskRepository.java
+++ src/main/java/efs/task/todoapp/repository/TaskRepository.java
@@ -1,8 +1,6 @@
 package efs.task.todoapp.repository;
 
-import java.util.List;
 import java.util.UUID;
-import java.util.function.Predicate;
 
 public class TaskRepository implements Repository<UUID, TaskEntity> {
 }
--- src/main/java/efs/task/todoapp/repository/UserRepository.java
+++ src/main/java/efs/task/todoapp/repository/UserRepository.java
@@ -1,7 +1,4 @@
 package efs.task.todoapp.repository;
 
-import java.util.List;
-import java.util.function.Predicate;
-
 public class UserRepository implements Repository<String, UserEntity> {
 }
